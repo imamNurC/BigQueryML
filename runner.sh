9ceec06b77b45baa55207216e14246f5e472f0e5
Mr.Ackerman <imamnurcakra@gmail.com>
2024-05-28 19:06:39 +0700

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -2,10 +2,12 @@
 
 runner1 =  "GetShotDistance.sql"
 runner2 =  "GetShotAngle.sql"
+runner3 = "model1.sql"
 
 
 bq query --use_legacy_sql=false  < "$runner1"
 bq query --use_legacy_sql=false  < "$runner2"
+bq query --use_legacy_sql=false  < "$runner3"
 
 
 
