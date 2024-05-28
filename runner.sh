be8fb63147f2eaa855149b793c1842b2bb8e325e
Mr.Ackerman <imamnurcakra@gmail.com>
2024-05-28 19:36:37 +0700

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-runner1 =  "GetShotDistance.sql"
-runner2 =  "GetShotAngle.sql"
-runner3 = "model1.sql"
-
+runner1 =  "https://raw.githubusercontent.com/imamNurC/BigQueryML/main/GetShotDistance.sql"
+runner2 =  "https://raw.githubusercontent.com/imamNurC/BigQueryML/main/GetShotAngle.sql"
+runner3 = "https://raw.githubusercontent.com/imamNurC/BigQueryML/main/model1.sql"
+runner4 =  "https://raw.githubusercontent.com/imamNurC/BigQueryML/main/"
+runner5 = "https://raw.githubusercontent.com/imamNurC/BigQueryML/main/"
 
 bq query --use_legacy_sql=false  < "$runner1"
 bq query --use_legacy_sql=false  < "$runner2"
 bq query --use_legacy_sql=false  < "$runner3"
+bq query --use_legacy_sql=false  < "$runner4"
+bq query --use_legacy_sql=false  < "$runner5"
+bq query --use_legacy_sql=false  < "$runner6"
+
 
 
 
