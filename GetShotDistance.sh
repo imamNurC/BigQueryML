8c59a47cb9d1e892a1d6c2a8f95a24280785a8f0
Mr.Ackerman <imamnurcakra@gmail.com>
2024-05-28 18:45:59 +0700

    ML big query

---
+++ GetShotDistance.sh
@@ -0,0 +1,18 @@
+
+#!/bin/bash
+
+# Task 2. Calculate shot distance and shot angle
+
+bq query --use_legacy_sql=false "
+
+CREATE FUNCTION \`soccer.GetShotDistanceToGoal\`(x INT64, y INT64)
+RETURNS FLOAT64
+AS (
+ /* Translate 0-100 (x,y) coordinate-based distances to absolute positions
+ using "average" field dimensions of 105x68 before combining in 2D dist calc */
+ SQRT(
+   POW((100 - x) * 105/100, 2) +
+   POW((50 - y) * 68/100, 2)
+   )
+ );
+"
