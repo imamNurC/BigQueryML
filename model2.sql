9bedc385d36281c23870afe81c396d3aa55cb4d9
Mr.Ackerman <imamnurcakra@gmail.com>
2024-05-28 19:39:08 +0700

    Create model2.sql

---
+++ model2.sql
@@ -0,0 +1,37 @@
+CREATE MODEL `soccer.xg_boosted_tree_model`
+OPTIONS(
+ model_type = 'BOOSTED_TREE_CLASSIFIER',
+ input_label_cols = ['isGoal']
+ ) AS
+
+SELECT
+ Events.subEventName AS shotType,
+
+ /* 101 is known Tag for 'goals' from goals table */
+ (101 IN UNNEST(Events.tags.id)) AS isGoal,
+  `soccer.GetShotDistanceToGoal`(Events.positions[ORDINAL(1)].x,
+   Events.positions[ORDINAL(1)].y) AS shotDistance,
+
+ `soccer.GetShotAngleToGoal`(Events.positions[ORDINAL(1)].x,
+   Events.positions[ORDINAL(1)].y) AS shotAngle
+
+FROM
+ `soccer.events` Events
+
+LEFT JOIN
+ `soccer.matches` Matches ON
+   Events.matchId = Matches.wyId
+
+LEFT JOIN
+ `soccer.competitions` Competitions ON
+   Matches.competitionId = Competitions.wyId
+
+WHERE
+ /* Filter out World Cup matches for model fitting purposes */
+ Competitions.name != 'World Cup' AND
+ /* Includes both "open play" & free kick shots (including penalties) */
+ (
+   eventName = 'Shot' OR
+   (eventName = 'Free Kick' AND subEventName IN ('Free kick shot', 'Penalty'))
+ )
+;
