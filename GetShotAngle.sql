d4ba289aa2fe2fcaecb8ec5b003d17808d18ed87
Mr.Ackerman <imamnurcakra@gmail.com>
2024-05-28 19:01:14 +0700

    Create GetShotAngle.sql

---
+++ GetShotAngle.sql
@@ -0,0 +1,25 @@
+CREATE FUNCTION \`soccer.GetShotAngleToGoal\`(x INT64, y INT64)
+RETURNS FLOAT64
+AS (
+ SAFE.ACOS(
+   /* Have to translate 0-100 (x,y) coordinates to absolute positions using
+   "average" field dimensions of 105x68 before using in various distance calcs */
+   SAFE_DIVIDE(
+     ( /* Squared distance between shot and 1 post, in meters */
+       (POW(105 - (x * 105/100), 2) + POW(34 + (7.32/2) - (y * 68/100), 2)) +
+       /* Squared distance between shot and other post, in meters */
+       (POW(105 - (x * 105/100), 2) + POW(34 - (7.32/2) - (y * 68/100), 2)) -
+       /* Squared length of goal opening, in meters */
+       POW(7.32, 2)
+     ),
+     (2 *
+       /* Distance between shot and 1 post, in meters */
+       SQRT(POW(105 - (x * 105/100), 2) + POW(34 + 7.32/2 - (y * 68/100), 2)) *
+       /* Distance between shot and other post, in meters */
+       SQRT(POW(105 - (x * 105/100), 2) + POW(34 - 7.32/2 - (y * 68/100), 2))
+     )
+    )
+  /* Translate radians to degrees */
+  ) * 180 / ACOS(-1)
+ )
+;
