67957cb661a18397abfbce915505b22307386de2
Mr.Ackerman <imamnurcakra@gmail.com>
2024-05-28 19:38:45 +0700

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -3,15 +3,17 @@
 runner1 =  "https://raw.githubusercontent.com/imamNurC/BigQueryML/main/GetShotDistance.sql"
 runner2 =  "https://raw.githubusercontent.com/imamNurC/BigQueryML/main/GetShotAngle.sql"
 runner3 = "https://raw.githubusercontent.com/imamNurC/BigQueryML/main/model1.sql"
-runner4 =  "https://raw.githubusercontent.com/imamNurC/BigQueryML/main/"
+runner4 =  "https://raw.githubusercontent.com/imamNurC/BigQueryML/main/model2.sql"
 runner5 = "https://raw.githubusercontent.com/imamNurC/BigQueryML/main/"
 
 bq query --use_legacy_sql=false  < "$runner1"
 bq query --use_legacy_sql=false  < "$runner2"
 bq query --use_legacy_sql=false  < "$runner3"
+bq query --use_legacy_sql=false  "SELECT * FROM ML.WEIGHTS(MODEL soccer.xg_logistic_reg_model);"
+
+
 bq query --use_legacy_sql=false  < "$runner4"
 bq query --use_legacy_sql=false  < "$runner5"
-bq query --use_legacy_sql=false  < "$runner6"
 
 
 
